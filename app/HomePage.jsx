bfc9af381abc83691a7ab566c8b9bd58b481ce9c
Munnaza <lukesh@daikimedia.com>
2026-07-02 13:54:16 +0500

    phase 1 for lcp created heroline svg

--- app/HomePage.jsx
+++ app/HomePage.jsx
@@ -1,37 +1,7 @@
-"use client";
-
-import dynamic from "next/dynamic";
 import Image from "next/image";
 import Hero from "@/components/home-1/Hero";
-
-
-// Lazy imports
-const Footer = dynamic(() => import("@/components/footer/Footer"));
-const Cta = dynamic(() => import("@/components/home-1/Cta"));
-const Integration = dynamic(() => import("@/components/home-1/Integration"));
-const Solution = dynamic(() => import("@/components/home-1/Solution"));
-const Vision = dynamic(() => import("@/components/home-1/Vision"));
-const RegionalStory = dynamic(() => import("@/components/home-1/RegionalStory"));
-const MarketsWeServe = dynamic(() => import("@/components/home-1/MarketsWeServe"));
-const WhoWeHelp = dynamic(() => import("@/components/home-1/WhoWeHelp"));
-const WhyChooseUs = dynamic(() => import("@/components/home-1/WhyChooseUs"));
-const WhatClientsValue  = dynamic(() => import("@/components/home-1/WhatClientsValue"));
-const LazyFaq = dynamic(() => import("@/components/lazy/LazyFaq"));
-const LazyCounter = dynamic(() => import("@/components/lazy/LazyCounter"));
-const LazyClients = dynamic(() => import("@/components/lazy/LazyClients"));
-const FinancialBlog = dynamic(
-  () => import("@/components/shared/FinancialBlog")
-);
-const Services = dynamic(() => import("@/components/shared/Services"));
-// const CustomSectionOne = dynamic(
-//   () => import("@/components/customSections/customSectionOne")
-// );
-const ContentCards = dynamic(
-  () => import("@/components/customSections/customContentCards")
-);
-const ComprehensiveServicesShowcase = dynamic(
-  () => import("@/components/customSections/comprehensiveServicesShowcase")
-);
+import BelowFoldClient from "@/components/home-1/BelowFoldClient";
+import FooterClient from "@/components/home-1/FooterClient";
 
 export default function HomePage() {
   return (
@@ -67,37 +37,10 @@ export default function HomePage() {
             />
           </div>
         </div>
-        
 
-        <RegionalStory />
-        <LazyClients 
-          title="Trusted By Brands Ready to Grow Smarter"
-          description={
-            <>
-              We work with brands that want more than random marketing activity.<br />
-              They want structure.<br />
-              They want consistency.<br />
-              They want better outcomes from the work they are already investing in.
-            </>
-          }
-          />
-        <Services />
-        <MarketsWeServe />
-        <WhoWeHelp />
-        <WhyChooseUs />
-        <LazyCounter />
-        <Vision />
-        <Solution />
-        <Integration />
-        {/* <CustomSectionOne /> */}
-        <WhatClientsValue />
-        <ContentCards />
-        <ComprehensiveServicesShowcase />
-        <LazyFaq />
-        <Cta />
-        <FinancialBlog />
+        <BelowFoldClient />
       </main>
-      <Footer />
+      <FooterClient />
     </>
   );
 }
